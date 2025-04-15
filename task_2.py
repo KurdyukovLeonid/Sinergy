b726efa7a817821c410e718876a1f161374f9c87
KurdyukovLeonid <leonid.kurdyukov@icloud.com>
2025-04-15 23:43:43 +0300

    2 задача.

---
+++ task_2.py
@@ -0,0 +1,7 @@
+stage_1 = input('Введите 1 стадию человека: ') #Homo neanderthalensis
+stage_2 = input('Введите 2 стадию человека: ') #Homo heidelbergensis
+stage_3 = input('Введите 3 стадию человека: ') #Homo erectus
+stage_4 = input('Введите 4 стадию человека: ') #Homo habilis
+stage_5 = input('Введите 5 стадию человека: ') #Homo sapiens sapiens
+
+print(stage_1, stage_2, stage_3, stage_4, stage_5, sep='=>')
